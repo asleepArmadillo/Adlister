5b5b368957ac5ee58d97e2797e75633bbeeef2db
j-womack <womack.joshua@gmail.com>
2015-08-10 13:55:48 -0500

    HTML templating

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+
+?>
+
+<html>
+<head>
+    <title>Adlister</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+    <header></header>
+
+    <? include "../views/partials/navbar.php"; ?>
+
+
+    
+
+    <footer></footer>
+</body>
+</html>
